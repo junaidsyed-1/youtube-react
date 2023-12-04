86e859d6d068b151a080cb87b7c0c9a4a4afd010
junaid <jaanusd79@gmail.com>
2023-12-04 16:22:57 +0530

    completed Responsive Navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import MenuIcon from '@mui/icons-material/Menu';
+import PersonIcon from '@mui/icons-material/Person';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import SearchIcon from '@mui/icons-material/Search';
 
 const Header = () => {
     return (
-        <div>
-            <h2>Header</h2>
+        <div className="grid grid-flow-col p-5 shadow-lg">
+
+            <div className="flex col-span-1 items-center">
+                <MenuIcon className="cursor-pointer" />
+                <img
+                    className='hidden md:block w-28 ml-4'
+                    src="https://upload.wikimedia.org/wikipedia/commons/b/b8/YouTube_Logo_2017.svg" alt="logo" />
+            </div>
+
+            <div className="col-span-10 flex items-center justify-center ">
+                <input className='w-1/2 border border-gray-400  py-2 px-4  rounded-l-full' type="text" />
+                <div className='border border-gray-400 py-2 px-4 rounded-r-full cursor-pointer'><SearchIcon /></div>
+            </div>
+
+            <div className="col-span-1 md:flex items-center justify-end hidden">
+                <NotificationsIcon className=' cursor-pointer' />
+                <PersonIcon className='mx-4 cursor-pointer' />
+            </div>
         </div>
     )
 }

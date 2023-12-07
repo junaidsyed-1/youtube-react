0aa65b56636b8657875b5a8e861692653f981f5e
junaid <jaanusd79@gmail.com>
2023-12-07 18:19:45 +0530

    sidebar functionality with redux

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,13 +2,24 @@ import MenuIcon from '@mui/icons-material/Menu';
 import PersonIcon from '@mui/icons-material/Person';
 import NotificationsIcon from '@mui/icons-material/Notifications';
 import SearchIcon from '@mui/icons-material/Search';
+import { useDispatch } from 'react-redux';
+import { toggleMenu } from '../utils/appSlice';
 
 const Header = () => {
+
+    const dispatch = useDispatch();
+
+    const toggleMenuHandler = () => {
+        dispatch(toggleMenu())
+    }
+
     return (
         <div className="grid grid-flow-col p-5 shadow-lg">
 
             <div className="flex col-span-1 items-center">
-                <MenuIcon className="cursor-pointer" />
+                <MenuIcon
+                    onClick={() => toggleMenuHandler()}
+                    className="cursor-pointer" />
                 <img
                     className='hidden md:block w-28 ml-4'
                     src="https://upload.wikimedia.org/wikipedia/commons/b/b8/YouTube_Logo_2017.svg" alt="logo" />
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,9 +1,35 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const Sidebar = () => {
+
+    const toggleMenu = useSelector(store => store.app.isMenuOpen)
+
+    if (!toggleMenu) return null;
+
     return (
-        <div>
-            <h3>SideBar</h3>
+        <div className="w-48">
+            <div>
+                <h1 className='font-bold pt-3'>You</h1>
+                <ul className='leading-8'>
+                    <li>Your Channel</li>
+                    <li>History</li>
+                    <li>Your Videos</li>
+                    <li>Watch Later</li>
+                </ul>
+                <hr className="mt-2" />
+            </div>
+            <div>
+                <h1 className='font-bold pt-3'>Subscriptions</h1>
+                <ul className='leading-8'>
+                    <li>Music</li>
+                    <li>Movies</li>
+                    <li>Live</li>
+                    <li>Gaming</li>
+                </ul>
+                <hr className="mt-2" />
+
+            </div>
         </div>
     )
 }

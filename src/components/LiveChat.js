8632b836dfcf8b557afa471d34e45a7ca0447b41
junaid <jaanusd79@gmail.com>
2023-12-15 22:35:12 +0530

    Updated

--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -1,10 +1,11 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import ChatMessage from "./ChatMessage"
 import { useDispatch, useSelector } from "react-redux"
 import { addMessage } from "../utils/chatSlice";
 import { generateRandomComments, generateRandomName } from "../utils/helper";
 
 const LiveChat = () => {
+    const [liveChatMessage, setLiveChatMessage] = useState("")
     const dispatch = useDispatch();
     const liveMessages = useSelector(store => store.chat.message)
 
@@ -24,12 +25,35 @@ const LiveChat = () => {
     }, []);
 
     return (
-        <div className="border border-black w-full bg-gray-100 h-[700px] mx-4 rounded-lg overflow-y-scroll flex flex-col-reverse">
-            <div className="py-1">
+        <div className="box-border w-full">
+            <div className="py-1 border bg-gray-100 mx-4 rounded-t-lg">
                 <h1 className="text-center text-lg font-bold block">Live Chat</h1>
             </div>
-            <hr className="border border-black" />
-            {liveMessages.map((m, i) => <ChatMessage key={i} name={m.name} message={m.message} />)}
+            <div className="border bg-gray-100 h-[620px] mx-4 rounded-b-lg overflow-y-scroll flex flex-col-reverse">
+
+                <hr className="border border-black" />
+                {liveMessages.map((m, i) => <ChatMessage key={i} name={m.name} message={m.message} />)}
+            </div>
+            <div className="mx-4 my-3 ">
+                <form
+                    onSubmit={(event) => {
+                        event.preventDefault();
+                        dispatch(addMessage({
+                            name: "Junaid Syed✔",
+                            message: liveChatMessage
+                        }));
+                        setLiveChatMessage("")
+                    }}
+                    className="flex justify-between">
+                    <input
+                        value={liveChatMessage}
+                        onChange={(e) => {
+                            setLiveChatMessage(e.target.value)
+                        }}
+                        type="text" className="border border-black py-1 px-4 rounded-lg w-4/5" />
+                    <button className="bg-[#FF0000] text-white px-3 py-1 rounded-lg mx-2">Send</button>
+                </form>
+            </div>
         </div>
     )
 }

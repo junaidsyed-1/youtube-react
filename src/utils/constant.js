dbcff57ef0539255ae5dff5eb7af6cad30fb5335
junaid <jaanusd79@gmail.com>
2023-12-18 21:34:16 +0530

    Updated

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -3,7 +3,7 @@ const API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY
 
 export const YOUTUBE_VIDEO_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=25&regionCode=IN&key=" + API_KEY
 
-export const YOUTUBE_SEARCH_API = "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+export const YOUTUBE_SEARCH_API = "https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
 
 export const COMMENTS_API = "https://jsonplaceholder.typicode.com/comments";
 
